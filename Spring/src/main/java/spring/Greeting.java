6ed2a92665832cd583c75e0d5cee34d74cc26058
Mateusz Tasz <mateusz.tasz@gmail.com>
2017-10-02 21:37:12 +0200

    add spring project

---
+++ Spring/src/main/java/spring/Greeting.java
@@ -0,0 +1,24 @@
+package spring;
+
+/**
+ * Created by Mateusz on 02.10.2017.
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(final long id, final String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ Spring/src/main/java/spring/GreetingController.java
@@ -0,0 +1,22 @@
+package spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Mateusz on 02.10.2017.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String hello = "Hello, ";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), hello + name);
+    }
+}
