a0fcb0351c9a502658f103217bb0acbb014158a8
Mateusz Tasz <mateusz.tasz@gmail.com>
2017-10-29 15:02:06 +0100

    add empty rxJava project

---
+++ RxJava/src/main/java/rxJava/Main.java
@@ -0,0 +1,14 @@
+package rxJava;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
+
+
+/**
+ * Created by Mateusz on 18.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
