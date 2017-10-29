eb590e9fbe845037a3fc8af5fd9295d4e031e3df
Mateusz Tasz <mateusz.tasz@gmail.com>
2017-10-29 18:03:47 +0100

    introduce java 8 and 9 feature modules

---
+++ Java__8__features/Lambda/src/main/java/lambda/Main.java
@@ -0,0 +1,17 @@
+package lambda;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+
+/**
+ * Created by Mateusz on 29.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java__9__features/FactoryMethods/src/main/java/factory/methods/Main.java
@@ -0,0 +1,14 @@
+package factory.methods;
+
+
+import java.util.List;
+
+/**
+ * Created by Mateusz on 29.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List list = List.of("elem 1", "elem2");
+        System.out.println(list);
+    }
+}
