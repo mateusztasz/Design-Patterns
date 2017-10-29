7357e5ba634354b55786a4ab6131e694c5b85deb
Mateusz Tasz <mateusz.tasz@gmail.com>
2017-10-29 13:49:11 +0100

    Spring: add swagger to project visible only in local profile

--- Spring/src/main/java/spring/Main.java
+++ Spring/src/main/java/spring/Main.java
@@ -1,21 +1,13 @@
 package spring;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 //@Controller
 //@EnableAutoConfiguration
 @SpringBootApplication
 public class Main {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World Mateusz!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
---
+++ Spring/src/main/java/spring/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Mateusz on 28.10.2017.
+ */
+
+
+@Configuration
+@EnableSwagger2
+@Profile(value = "local")
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("greetings")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+    }
+}
